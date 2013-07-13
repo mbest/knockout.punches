6ab5c78c1c613fe3dc2711ce86f53eaa389d65d0
Michael Best <mbest@dasya.com>
2013-07-13 13:22:59 -1000

    clean up wrappedCallback

--- src/wrappedCallback.js
+++ src/wrappedCallback.js
@@ -1,17 +1,8 @@
-function isIdentifierOrProperty(expression) {
-    // Matches either an isolated identifier or something ending with a property accessor
-    return /^([$_a-z][$\w]*|.+(\.\s*[$_a-z][$\w]*|\[.+\]))$/i.test(expression);
-}
-
-function isFunctionLiteral(expression) {
-    // Match a function literal, which start with "function" end with a parenthesis
-    return /^[\(\s]*function\s*\(.*}[\)\s]*$/.test(expression);
-}
-
+// Wrap a callback function in an anonymous function so that it is called with the appropriate
+// "this" value.
 function wrappedCallbackPreprocessor(val) {
-    if (!isFunctionLiteral(val) && isIdentifierOrProperty(val)) {
-        // call function literal in an anonymous function so that it is called
-        // with appropriate "this" value
+    // Matches either an isolated identifier or something ending with a property accessor
+    if (/^([$_a-z][$\w]*|.+(\.\s*[$_a-z][$\w]*|\[.+\]))$/i.test(val)) {
         return 'function(_x,_y,_z){return(' + val + ')(_x,_y,_z);}';
     } else {
         return val;

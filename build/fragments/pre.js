c239d87ae826c0a8a0b964a54837cabb179a610b
Michael Best <mbest@dasya.com>
2013-07-26 17:53:05 -1000

    update comment in header

--- build/fragments/pre.js
+++ build/fragments/pre.js
@@ -1,6 +1,6 @@
 /**
  * @license Knockout.punches
- * Extended binding syntaxes for Knockout
+ * Enhanced binding syntaxes for Knockout 3+
  * (c) Michael Best
  * License: MIT (http://www.opensource.org/licenses/mit-license.php)
  * Version 0.1.0

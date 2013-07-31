c233eaa56b7933d4fee43ba37f6515f6f3ce3b51
Michael Best <mbest@dasya.com>
2013-07-30 15:37:35 -1000

    Update page; add built files for download

---
+++ knockout.punches.js
@@ -0,0 +1,412 @@
+/**
+ * @license Knockout.punches
+ * Enhanced binding syntaxes for Knockout 3+
+ * (c) Michael Best
+ * License: MIT (http://www.opensource.org/licenses/mit-license.php)
+ * Version 0.1.0
+ */
+(function() {
+// Add a preprocess funtion to a binding handler.
+function setBindingPreprocessor(bindingKeyOrHandler, preprocessFn) {
+    chainPreprocessor(getOrCreateHandler(bindingKeyOrHandler), 'preprocess', preprocessFn);
+}
+
+// These utility functions are separated out because they're also used by
+// preprocessBindingProperty
+
+// Get the binding handler or create a new, empty one
+function getOrCreateHandler(bindingKeyOrHandler) {
+    return typeof bindingKeyOrHandler === 'object' ? bindingKeyOrHandler :
+        (ko.getBindingHandler(bindingKeyOrHandler) || (ko.bindingHandlers[bindingKeyOrHandler] = {}));
+}
+// Add a preprocess function
+function chainPreprocessor(obj, prop, fn) {
+    if (obj[prop]) {
+        // If the handler already has a preprocess function, chain the new
+        // one after the existing one. If the previous function in the chain
+        // returns a falsy value (to remove the binding), the chain ends. This
+        // method allows each function to modify and return the binding value.
+        var previousFn = obj[prop];
+        obj[prop] = function(value, binding, addBinding) {
+            value = previousFn.call(this, value, binding, addBinding);
+            if (value)
+                return fn.call(this, value, binding, addBinding);
+        };
+    } else {
+        obj[prop] = fn;
+    }
+}
+
+// Add a preprocessNode function to the binding provider. If a
+// function already exists, chain the new one after it. This calls
+// each function in the chain until one modifies the node. This
+// method allows only one function to modify the node.
+function setNodePreprocessor(preprocessFn) {
+    var provider = ko.bindingProvider.instance;
+    if (provider.preprocessNode) {
+        var previousPreprocessFn = provider.preprocessNode;
+        provider.preprocessNode = function(node) {
+            var newNodes = previousPreprocessFn.call(this, node);
+            if (!newNodes)
+                newNodes = preprocessFn.call(this, node);
+            return newNodes;
+        };
+    } else {
+        provider.preprocessNode = preprocessFn;
+    }
+}
+
+// Create "punches" object and export utility functions
+ko.punches = {
+    utils: {
+        setBindingPreprocessor: setBindingPreprocessor,
+        setNodePreprocessor: setNodePreprocessor
+    }
+};
+// Convert input in the form of `expression | filter1 | filter2:arg1:arg2` to a function call format
+// with filters accessed as ko.filters.filter1, etc.
+function filterPreprocessor(input) {
+    // Check if the input contains any | characters; if not, just return
+    if (input.indexOf('|') === -1)
+        return input;
+
+    // Split the input into tokens, in which | and : are individual tokens, quoted strings are ignored, and all tokens are space-trimmed
+    var tokens = input.match(/"([^"\\]|\\.)*"|'([^'\\]|\\.)*'|\|\||[|:]|[^\s|:"'][^|:"']*[^\s|:"']|[^\s|:"']/g);
+    if (tokens && tokens.length > 1) {
+        // Append a line so that we don't need a separate code block to deal with the last item
+        tokens.push('|');
+        input = tokens[0];
+        var lastToken, token, inFilters = false, nextIsFilter = false;
+        for (var i = 1, token; token = tokens[i]; ++i) {
+            if (token === '|') {
+                if (inFilters) {
+                    if (lastToken === ':')
+                        input += "undefined";
+                    input += ')';
+                }
+                nextIsFilter = true;
+                inFilters = true;
+            } else {
+                if (nextIsFilter) {
+                    input = "ko.filters['" + token + "'](" + input;
+                } else if (inFilters && token === ':') {
+                    if (lastToken === ':')
+                        input += "undefined";
+                    input += ",";
+                } else {
+                    input += token;
+                }
+                nextIsFilter = false;
+            }
+            lastToken = token;
+        }
+    }
+    return input;
+}
+
+// Set the filter preprocessor for a specific binding
+function enableTextFilter(bindingKeyOrHandler) {
+    setBindingPreprocessor(bindingKeyOrHandler, filterPreprocessor);
+}
+
+var filters = {};
+
+// Convert value to uppercase
+filters.uppercase = function(value) {
+    return String.prototype.toUpperCase.call(value);
+};
+
+// Convert value to lowercase
+filters.lowercase = function(value) {
+    return String.prototype.toLowerCase.call(value);
+};
+
+// Return default value if the input value is blank or null
+filters['default'] = function(value, defaultValue) {
+    return (value === '' || value == null) ? defaultValue : value;
+};
+
+// Return the value with the search string replaced with the replacement string
+filters.replace = function(value, search, replace) {
+    return String.prototype.replace.call(value, search, replace);
+};
+
+filters.fit = function(value, length, replacement, trimWhere) {
+    if (length && ('' + value).length > length) {
+        replacement = '' + (replacement || '...');
+        length = length - replacement.length;
+        value = '' + value;
+        switch (trimWhere) {
+            case 'left':
+                return replacement + value.slice(-length);
+            case 'middle':
+                var leftLen = Math.ceil(length / 2);
+                return value.substr(0, leftLen) + replacement + value.slice(leftLen-length);
+            default:
+                return value.substr(0, length) + replacement;
+        }
+    } else {
+        return value;
+    }
+};
+
+// Convert a model object to JSON
+filters.json = function(rootObject, space, replacer) {     // replacer and space are optional
+    return ko.toJSON(rootObject, replacer, space);
+};
+
+// Format a number using the browser's toLocaleString
+filters.number = function(value) {
+    return (+value).toLocaleString();
+};
+
+// Export the filters object for general access
+ko.filters = filters;
+
+// Export the preprocessor functions
+ko.punches.textFilter = {
+    preprocessor: filterPreprocessor,
+    enableForBinding: enableTextFilter
+};
+// Support dynamically-created, namespaced bindings. The binding key syntax is
+// "namespace.binding". Within a certain namespace, we can dynamically create the
+// handler for any binding. This is particularly useful for bindings that work
+// the same way, but just set a different named value, such as for element
+// attributes or CSS classes.
+var namespacedBindingMatch = /([^\.]+)\.(.+)/, namespaceDivider = '.';
+function createNamespacedBindingHandler(bindingKey) {
+    var match = bindingKey.match(namespacedBindingMatch);
+    if (match) {
+        var namespace = match[1],
+            namespaceHandler = ko.bindingHandlers[namespace];
+        if (namespaceHandler) {
+            var bindingName = match[2],
+                handlerFn = namespaceHandler.getNamespacedHandler || defaultGetNamespacedHandler,
+                handler = handlerFn.call(namespaceHandler, bindingName, namespace, bindingKey);
+            ko.bindingHandlers[bindingKey] = handler;
+            return handler;
+        }
+    }
+}
+
+// Knockout's built-in bindings "attr", "event", "css" and "style" include the idea of
+// namespaces, representing it using a single binding that takes an object map of names
+// to values. This default handler translates a binding of "namespacedName: value"
+// to "namespace: {name: value}" to automatically support those built-in bindings.
+function defaultGetNamespacedHandler(name, namespace, namespacedName) {
+    var handler = ko.utils.extend({}, this);
+    function setHandlerFunction(funcName) {
+        if (handler[funcName]) {
+            handler[funcName] = function(element, valueAccessor) {
+                function subValueAccessor() {
+                    var result = {};
+                    result[name] = valueAccessor();
+                    return result;
+                }
+                var args = Array.prototype.slice.call(arguments, 0);
+                args[1] = subValueAccessor;
+                return ko.bindingHandlers[namespace][funcName].apply(this, args);
+            };
+        }
+    }
+    // Set new init and update functions that wrap the originals
+    setHandlerFunction('init');
+    setHandlerFunction('update');
+    // Clear any preprocess function since preprocessing of the new binding would need to be different
+    if (handler.preprocess)
+        handler.preprocess = null;
+    if (ko.virtualElements.allowedBindings[namespace])
+        ko.virtualElements.allowedBindings[namespacedName] = true;
+    return handler;
+}
+
+// Sets a preprocess function for every generated namespace.x binding. This can
+// be called multiple times for the same binding, and the preprocess functions will
+// be chained. If the binding has a custom getNamespacedHandler method, make sure that's
+// set before this function is used.
+function setDefaultNamespacedBindingPreprocessor(namespace, preprocessFn) {
+    var handler = ko.getBindingHandler(namespace);
+    if (handler) {
+        var previousHandlerFn = handler.getNamespacedHandler || defaultGetNamespacedHandler;
+        handler.getNamespacedHandler = function() {
+            return setBindingPreprocessor(previousHandlerFn.apply(this, arguments), preprocessFn);
+        };
+    }
+}
+
+// ko.getBindingHandler will dynmically create namespaced bindings
+var oldGetHandler = ko.getBindingHandler;
+ko.getBindingHandler = function(bindingKey) {
+    return oldGetHandler(bindingKey) || createNamespacedBindingHandler(bindingKey);
+};
+
+function autoNamespacedPreprocessor(value, binding, addBinding) {
+    if (value.charAt(0) !== "{")
+        return value;
+
+    // Handle two-level binding specified as "binding: {key: value}" by parsing inner
+    // object and converting to "binding.key: value"
+    var subBindings = ko.expressionRewriting.parseObjectLiteral(value);
+    ko.utils.arrayForEach(subBindings, function(keyValue) {
+        addBinding(binding + namespaceDivider + keyValue.key, keyValue.value);
+    });
+}
+
+// Set the namespaced preprocessor for a specific binding
+function enableAutoNamespacedSyntax(bindingKeyOrHandler) {
+    setBindingPreprocessor(bindingKeyOrHandler, autoNamespacedPreprocess);
+}
+
+// Export the preprocessor functions
+ko.punches.namespacedBinding = {
+    defaultGetHandler: defaultGetNamespacedHandler,
+    setDefaultBindingPreprocessor: setDefaultNamespacedBindingPreprocessor,
+    preprocessor: autoNamespacedPreprocessor,
+    enableForBinding: enableAutoNamespacedSyntax
+};
+// Wrap a callback function in an anonymous function so that it is called with the appropriate
+// "this" value.
+function wrappedCallbackPreprocessor(val) {
+    // Matches either an isolated identifier or something ending with a property accessor
+    if (/^([$_a-z][$\w]*|.+(\.\s*[$_a-z][$\w]*|\[.+\]))$/i.test(val)) {
+        return 'function(_x,_y,_z){return(' + val + ')(_x,_y,_z);}';
+    } else {
+        return val;
+    }
+}
+
+// Set the wrappedCallback preprocessor for a specific binding
+function enableWrappedCallback(bindingKeyOrHandler) {
+    setBindingPreprocessor(bindingKeyOrHandler, wrappedCallbackPreprocessor);
+}
+
+// Export the preprocessor functions
+ko.punches.wrappedCallback = {
+    preprocessor: wrappedCallbackPreprocessor,
+    enableForBinding: enableWrappedCallback
+};
+// Attach a preprocess function to a specific property of a binding. This allows you to
+// preprocess binding "options" using the same preprocess functions that work for bindings.
+function setBindingPropertyPreprocessor(bindingKeyOrHandler, property, preprocessFn) {
+    var handler = getOrCreateHandler(bindingKeyOrHandler);
+    if (!handler._propertyPreprocessors) {
+        // Initialize the binding preprocessor
+        chainPreprocessor(handler, 'preprocess', propertyPreprocessor);
+        handler._propertyPreprocessors = {};
+    }
+    // Add the property preprocess function
+    chainPreprocessor(handler._propertyPreprocessors, property, preprocessFn);
+}
+
+// In order to preprocess a binding property, we have to preprocess the binding itself.
+// This preprocess function splits up the binding value and runs each property's preprocess
+// function if it's set.
+function propertyPreprocessor(value, binding, addBinding) {
+    if (value.charAt(0) !== "{")
+        return value;
+
+    var subBindings = ko.expressionRewriting.parseObjectLiteral(value),
+        resultStrings = [],
+        propertyPreprocessors = this._propertyPreprocessors || {};
+    ko.utils.arrayForEach(subBindings, function(keyValue) {
+        var prop = keyValue.key, propVal = keyValue.value;
+        if (propertyPreprocessors[prop]) {
+            propVal = propertyPreprocessors[prop](propVal, prop, addBinding);
+        }
+        if (propVal) {
+            resultStrings.push("'" + prop + "':" + propVal);
+        }
+    });
+    return "{" + resultStrings.join(",") + "}";
+}
+
+// Export the preprocessor functions
+ko.punches.preprocessBindingProperty = {
+    setPreprocessor: setBindingPropertyPreprocessor
+};
+// Wrap an expression in an anonymous function so that it is called when the event happens
+function makeExpressionCallbackPreprocessor(args) {
+    return function expressionCallbackPreprocessor(val) {
+        return 'function('+args+'){return(' + val + ');}';
+    };
+}
+
+var eventExpressionPreprocessor = makeExpressionCallbackPreprocessor("$data,$event");
+
+// Set the expressionCallback preprocessor for a specific binding
+function enableExpressionCallback(bindingKeyOrHandler, args) {
+    var args = Array.prototype.slice.call(arguments, 1).join();
+    setBindingPreprocessor(bindingKeyOrHandler, makeExpressionCallbackPreprocessor(args));
+}
+
+// Export the preprocessor functions
+ko.punches.expressionCallback = {
+    makePreprocessor: makeExpressionCallbackPreprocessor,
+    eventPreprocessor: eventExpressionPreprocessor,
+    enableForBinding: enableExpressionCallback
+};
+
+// Create an "on" namespace for events to use the expression method
+ko.bindingHandlers.on = {
+    getNamespacedHandler: function(eventName) {
+        var handler = ko.getBindingHandler('event' + namespaceDivider + eventName);
+        setBindingPreprocessor(handler, eventExpressionPreprocessor);
+        return handler;
+    }
+};
+// Performance comparison at http://jsperf.com/markup-interpolation-comparison
+function interpolationMarkupPreprocessor(node) {
+    // only needs to work with text nodes
+    if (node.nodeType === 3 && node.nodeValue && node.nodeValue.indexOf('{{') !== -1) {
+        var nodes = [];
+        function addTextNode(text) {
+            if (text)
+                nodes.push(document.createTextNode(text));
+        }
+        function wrapExpr(expressionText) {
+            nodes.push(document.createComment("ko text:" + expressionText));
+            nodes.push(document.createComment("/ko"));
+        }
+        function innerParse(text) {
+            var innerMatch = text.match(/^([\s\S]*?)}}([\s\S]*)\{\{([\s\S]*)$/);
+            if (innerMatch) {
+                wrapExpr(innerMatch[1]);
+                outerParse(innerMatch[2]);
+                wrapExpr(innerMatch[3]);
+            } else {
+                wrapExpr(text);
+            }
+        }
+        function outerParse(text) {
+            var outerMatch = text.match(/^([\s\S]*?)\{\{([\s\S]*)}}([\s\S]*)$/);
+            if (outerMatch) {
+                addTextNode(outerMatch[1]);
+                innerParse(outerMatch[2]);
+                addTextNode(outerMatch[3]);
+            } else {
+                addTextNode(text);
+            }
+        }
+        outerParse(node.nodeValue);
+        if (nodes.length > 1) {
+            if (node.parentNode) {
+                for (var i = 0; i < nodes.length; i++) {
+                    node.parentNode.insertBefore(nodes[i], node);
+                }
+                node.parentNode.removeChild(node);
+            }
+            return nodes;
+        }
+    }
+}
+
+function enableInterpolationMarkup() {
+    setNodePreprocessor(interpolationMarkupPreprocessor);
+}
+
+// Export the preprocessor functions
+ko.punches.interpolationMarkup = {
+    preprocessor: interpolationMarkupPreprocessor,
+    enable: enableInterpolationMarkup
+};
+}());

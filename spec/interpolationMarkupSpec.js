666dd653cca8c6ac783d0812b82f570d5d12e55d
Michael Best <mbest@dasya.com>
2014-01-13 09:29:12 -1000

    Add support for interpolation markup in attributes.

--- spec/interpolationMarkupSpec.js
+++ spec/interpolationMarkupSpec.js
@@ -1,11 +1,10 @@
 describe("Interpolation Markup preprocessor", function() {
-
-    it('Should return undefined when there are no bindings', function() {
+    it('Should do nothing when there are no expressions', function() {
         var result = ko.punches.interpolationMarkup.preprocessor(document.createTextNode("some text"));
         expect(result).toBeUndefined();
     });
 
-    it('Should return undefined when empty text', function() {
+    it('Should do nothing when empty', function() {
         var result = ko.punches.interpolationMarkup.preprocessor(document.createTextNode(""));
         expect(result).toBeUndefined();
     });
@@ -20,7 +19,7 @@ describe("Interpolation Markup preprocessor", function() {
         expect(result).toBeUndefined();
     });
 
-    it('Should replace {{...}} binding with new nodes', function() {
+    it('Should create binding from {{...}} expression', function() {
         var result = ko.punches.interpolationMarkup.preprocessor(document.createTextNode("some {{ expr }} text"));
         expect(result).toHaveNodeTypes([3, 8, 8, 3]);   // text, comment, comment, text
     });
@@ -30,17 +29,17 @@ describe("Interpolation Markup preprocessor", function() {
         expect(result).toHaveNodeTypes([3, 8, 8, 3]);   // text, comment, comment, text
     });
 
-    it('Should replace each binding with new nodes', function() {
+    it('Should support two expressions', function() {
         var result = ko.punches.interpolationMarkup.preprocessor(document.createTextNode("some {{ expr1 }} middle {{ expr2 }} text"));
         expect(result).toHaveNodeTypes([3, 8, 8, 3, 8, 8, 3]);   // text, comment, comment, text, comment, comment, text
     });
 
-    it('Should skip nodes if empty', function() {
+    it('Should skip empty text', function() {
         var result = ko.punches.interpolationMarkup.preprocessor(document.createTextNode("{{ expr1 }}{{ expr2 }}"));
         expect(result).toHaveNodeTypes([8, 8, 8, 8]);   // comment, comment, comment, comment
     });
 
-    it('Should support more than two bindings', function() {
+    it('Should support more than two expressions', function() {
         var result = ko.punches.interpolationMarkup.preprocessor(document.createTextNode("x {{ expr1 }} y {{ expr2 }} z {{ expr3 }}"));
         expect(result).toHaveNodeTypes([3, 8, 8, 3, 8, 8, 3, 8, 8]);   // text, comment, comment, text, comment, comment, text, comment, comment
     });
@@ -53,20 +52,20 @@ describe("Interpolation Markup bindings", function() {
     beforeEach(ko.punches.interpolationMarkup.enable);
     afterEach(function() { ko.bindingProvider.instance.preprocessNode = savePreprocessNode; });
 
-    it('Should replace {{...}} binding with virtual text binding', function() {
+    it('Should replace {{...}} expression with virtual text binding', function() {
         testNode.innerHTML = "hello {{'name'}}!";
         ko.applyBindings(null, testNode);
         expect(testNode).toContainText("hello name!");
         expect(testNode).toContainHtml("hello <!--ko text:'name'-->name<!--/ko-->!");
     });
 
-    it('Should replace multiple bindings', function() {
+    it('Should replace multiple expressions', function() {
         testNode.innerHTML = "hello {{'name'}}{{'!'}}";
         ko.applyBindings(null, testNode);
         expect(testNode).toContainText("hello name!");
     });
 
-    it('Should support any contents of binding, including functions and {{}}', function() {
+    it('Should support any content of expression, including functions and {{}}', function() {
         testNode.innerHTML = "hello {{ (function(){return '{{name}}'}()) }}!";
         ko.applyBindings(null, testNode);
         expect(testNode).toContainText("hello {{name}}!");
@@ -76,6 +75,15 @@ describe("Interpolation Markup bindings", function() {
         testNode.innerHTML = "hello }}'name'{{'!'}}{{";
         ko.applyBindings(null, testNode);
         expect(testNode).toContainText("hello }}'name'!{{");
+    });
+
+    it('Should update when observable changes', function() {
+        testNode.innerHTML = "The best {{what}}.";
+        var observable = ko.observable('time');
+        ko.applyBindings({what: observable}, testNode);
+        expect(testNode).toContainText("The best time.");
+        observable('fun');
+        expect(testNode).toContainText("The best fun.");
     });
 
     it('Should be able to override wrapExpresssion to define a different set of elements', function() {
@@ -91,5 +99,162 @@ describe("Interpolation Markup bindings", function() {
         testNode.innerHTML = "hello {{'name'}}!";
         ko.applyBindings(null, testNode);
         expect(testNode).toContainText("hello --name--!");
+    });
+});
+
+describe("Attribute Interpolation Markup preprocessor", function() {
+    var testNode;
+    beforeEach(function () {
+        testNode = document.createElement("div");
+    });
+
+    it('Should do nothing when there are no expressions', function() {
+        testNode.setAttribute('title', "some text");
+        ko.punches.attributeInterpolationMarkup.preprocessor(testNode);
+        expect(testNode.title).toEqual('some text');
+        expect(testNode.getAttribute('data-bind')).toBe(null);
+    });
+
+    it('Should do nothing when empty', function() {
+        testNode.setAttribute('title', "");
+        ko.punches.attributeInterpolationMarkup.preprocessor(testNode);
+        expect(testNode.title).toEqual('');
+        expect(testNode.getAttribute('data-bind')).toBe(null);
+    });
+
+    it('Should not parse unclosed binding', function() {
+        testNode.setAttribute('title', "some {{text");
+        ko.punches.attributeInterpolationMarkup.preprocessor(testNode);
+        expect(testNode.title).toEqual('some {{text');
+        expect(testNode.getAttribute('data-bind')).toBe(null);
+    });
+
+    it('Should not parse unopened binding', function() {
+        testNode.setAttribute('title', "some}} text");
+        ko.punches.attributeInterpolationMarkup.preprocessor(testNode);
+        expect(testNode.title).toEqual('some}} text');
+        expect(testNode.getAttribute('data-bind')).toBe(null);
+    });
+
+    it('Should create binding from {{...}} expression', function() {
+        testNode.setAttribute('title', "some {{expr}} text");
+        ko.punches.attributeInterpolationMarkup.preprocessor(testNode);
+        expect(testNode.title).toEqual('');
+        expect(testNode.getAttribute('data-bind')).toEqual('attr.title:""+"some "+ko.unwrap(expr)+" text"');
+    });
+
+    it('Should ignore unmatched delimiters', function() {
+        testNode.setAttribute('title', "some {{expr1}}expr2}} text");
+        ko.punches.attributeInterpolationMarkup.preprocessor(testNode);
+        expect(testNode.title).toEqual('');
+        expect(testNode.getAttribute('data-bind')).toEqual('attr.title:""+"some "+ko.unwrap(expr1}}expr2)+" text"');
+    });
+
+    it('Should support two expressions', function() {
+        testNode.setAttribute('title', "some {{expr1}} middle {{expr2}} text");
+        ko.punches.attributeInterpolationMarkup.preprocessor(testNode);
+        expect(testNode.title).toEqual('');
+        expect(testNode.getAttribute('data-bind')).toEqual('attr.title:""+"some "+ko.unwrap(expr1)+" middle "+ko.unwrap(expr2)+" text"');
+    });
+
+    it('Should skip empty text', function() {
+        testNode.setAttribute('title', "{{expr1}}{{expr2}}");
+        ko.punches.attributeInterpolationMarkup.preprocessor(testNode);
+        expect(testNode.title).toEqual('');
+        expect(testNode.getAttribute('data-bind')).toEqual('attr.title:""+ko.unwrap(expr1)+ko.unwrap(expr2)');
+    });
+
+    it('Should support more than two expressions', function() {
+        testNode.setAttribute('title', "x {{expr1}} y {{expr2}} z {{expr3}}");
+        ko.punches.attributeInterpolationMarkup.preprocessor(testNode);
+        expect(testNode.title).toEqual('');
+        expect(testNode.getAttribute('data-bind')).toEqual('attr.title:""+"x "+ko.unwrap(expr1)+" y "+ko.unwrap(expr2)+" z "+ko.unwrap(expr3)');
+    });
+
+    it('Should create simple binding for single expression', function() {
+        testNode.setAttribute('title', "{{expr1}}");
+        ko.punches.attributeInterpolationMarkup.preprocessor(testNode);
+        expect(testNode.title).toEqual('');
+        expect(testNode.getAttribute('data-bind')).toEqual('attr.title:expr1');
+    });
+
+    it('Should append to existing data-bind', function() {
+        testNode.setAttribute('title', "{{expr1}}");
+        testNode.setAttribute('data-bind', "text:expr2");
+        ko.punches.attributeInterpolationMarkup.preprocessor(testNode);
+        expect(testNode.title).toEqual('');
+        expect(testNode.getAttribute('data-bind')).toEqual('text:expr2,attr.title:expr1');
+    });
+
+    it('Should not match expressions in data-bind', function() {
+        testNode.setAttribute('data-bind', "text:'{{xyz}}'");
+        ko.punches.attributeInterpolationMarkup.preprocessor(testNode);
+        expect(testNode.getAttribute('data-bind')).toEqual("text:'{{xyz}}'");
+    });
+
+    it('Should support expressions in multiple attributes', function() {
+        testNode.setAttribute('title', "{{expr1}}");
+        testNode.setAttribute('class', "test");
+        testNode.setAttribute('id', "{{expr2}}");
+        testNode.setAttribute('data-test', "{{expr3}}");
+        ko.punches.attributeInterpolationMarkup.preprocessor(testNode);
+        expect(testNode.getAttribute('data-bind')).toEqual('attr.data-test:expr3,attr.id:expr2,attr.title:expr1'); // the order shouldn't matter
+    });
+});
+
+describe("Attribute Interpolation Markup bindings", function() {
+    beforeEach(jasmine.prepareTestNode);
+
+    var savePreprocessNode = ko.bindingProvider.instance.preprocessNode;
+    beforeEach(ko.punches.attributeInterpolationMarkup.enable);
+    afterEach(function() { ko.bindingProvider.instance.preprocessNode = savePreprocessNode; });
+
+    it('Should replace {{...}} expression in attribute', function() {
+        testNode.innerHTML = "<div title='hello {{\"name\"}}!'></div>";
+        ko.applyBindings(null, testNode);
+        expect(testNode.childNodes[0].title).toEqual("hello name!");
+    });
+
+    it('Should replace multiple expressions', function() {
+        testNode.innerHTML = "<div title='hello {{\"name\"}}{{\"!\"}}'></div>";
+        ko.applyBindings(null, testNode);
+        expect(testNode.childNodes[0].title).toEqual("hello name!");
+    });
+
+    it('Should support any content of expression, including functions and {{}}', function() {
+        testNode.innerHTML = "<div title='hello {{ (function(){return \"{{name}}\"}()) }}!'></div>";
+        ko.applyBindings(null, testNode);
+        expect(testNode.childNodes[0].title).toEqual("hello {{name}}!");
+    });
+
+    it('Should properly handle quotes in text sections', function() {
+        testNode.innerHTML = "<div title='This is \"great\" {{\"fun\"}} with &apos;friends&apos;'></div>";
+        ko.applyBindings(null, testNode);
+        expect(testNode.childNodes[0].title).toEqual("This is \"great\" fun with 'friends'");
+    });
+
+    it('Should ignore unmatched }} and {{', function() {
+        testNode.innerHTML = "<div title='hello }}\"name\"{{\"!\"}}{{'></div>";
+        ko.applyBindings(null, testNode);
+        expect(testNode.childNodes[0].title).toEqual("hello }}\"name\"!{{");
+    });
+
+    it('Should support expressions in multiple attributes', function() {
+        testNode.innerHTML = "<div title='{{title}}' id='{{id}}' class='test class' data-test='hello {{\"name\"}}!' data-bind='text:content'></div>";
+        ko.applyBindings({title: 'the title', id: 'test id', content: 'content'}, testNode);
+        expect(testNode).toContainText("content");
+        expect(testNode.childNodes[0].title).toEqual("the title");
+        expect(testNode.childNodes[0].id).toEqual("test id");
+        expect(testNode.childNodes[0].className).toEqual("test class");
+        expect(testNode.childNodes[0].getAttribute('data-test')).toEqual("hello name!");
+    });
+
+    it('Should update when observable changes', function() {
+        testNode.innerHTML = "<div title='The best {{what}}.'></div>";
+        var observable = ko.observable('time');
+        ko.applyBindings({what: observable}, testNode);
+        expect(testNode.childNodes[0].title).toEqual("The best time.");
+        observable('fun');
+        expect(testNode.childNodes[0].title).toEqual("The best fun.");
     });
 });
--- src/interpolationMarkup.js
+++ src/interpolationMarkup.js
@@ -1,4 +1,28 @@
 // Performance comparison at http://jsperf.com/markup-interpolation-comparison
+function parseInterpolationMarkup(textToParse, outerTextCallback, expressionCallback) {
+    function innerParse(text) {
+        var innerMatch = text.match(/^([\s\S]*?)}}([\s\S]*)\{\{([\s\S]*)$/);
+        if (innerMatch) {
+            expressionCallback(innerMatch[1]);
+            outerParse(innerMatch[2]);
+            expressionCallback(innerMatch[3]);
+        } else {
+            expressionCallback(text);
+        }
+    }
+    function outerParse(text) {
+        var outerMatch = text.match(/^([\s\S]*?)\{\{([\s\S]*)}}([\s\S]*)$/);
+        if (outerMatch) {
+            outerTextCallback(outerMatch[1]);
+            innerParse(outerMatch[2]);
+            outerTextCallback(outerMatch[3]);
+        } else {
+            outerTextCallback(text);
+        }
+    }
+    outerParse(textToParse);
+}
+
 function interpolationMarkupPreprocessor(node) {
     // only needs to work with text nodes
     if (node.nodeType === 3 && node.nodeValue && node.nodeValue.indexOf('{{') !== -1) {
@@ -8,29 +32,11 @@ function interpolationMarkupPreprocessor(node) {
                 nodes.push(document.createTextNode(text));
         }
         function wrapExpr(expressionText) {
-            nodes.push.apply(nodes, ko_punches_interpolationMarkup.wrapExpresssion(expressionText));
+            if (expressionText)
+                nodes.push.apply(nodes, ko_punches_interpolationMarkup.wrapExpresssion(expressionText));
         }
-        function innerParse(text) {
-            var innerMatch = text.match(/^([\s\S]*?)}}([\s\S]*)\{\{([\s\S]*)$/);
-            if (innerMatch) {
-                wrapExpr(innerMatch[1]);
-                outerParse(innerMatch[2]);
-                wrapExpr(innerMatch[3]);
-            } else {
-                wrapExpr(text);
-            }
-        }
-        function outerParse(text) {
-            var outerMatch = text.match(/^([\s\S]*?)\{\{([\s\S]*)}}([\s\S]*)$/);
-            if (outerMatch) {
-                addTextNode(outerMatch[1]);
-                innerParse(outerMatch[2]);
-                addTextNode(outerMatch[3]);
-            } else {
-                addTextNode(text);
-            }
-        }
-        outerParse(node.nodeValue);
+        parseInterpolationMarkup(node.nodeValue, addTextNode, wrapExpr)
+
         if (nodes.length > 1) {
             if (node.parentNode) {
                 for (var i = 0; i < nodes.length; i++) {
@@ -59,4 +65,52 @@ var ko_punches_interpolationMarkup = ko_punches.interpolationMarkup = {
     preprocessor: interpolationMarkupPreprocessor,
     enable: enableInterpolationMarkup,
     wrapExpresssion: wrapExpresssion
+};
+
+
+function attributeInterpolationMarkerPreprocessor(node) {
+    if (node.nodeType === 1 && node.attributes.length) {
+        var dataBindAttribute = node.getAttribute('data-bind');
+        for (var attrs = node.attributes, i = attrs.length-1; i >= 0; --i) {
+            var attr = attrs[i];
+            if (attr.specified && attr.name != 'data-bind' && attr.value.indexOf('{{') !== -1) {
+                var parts = [], attrBinding = 0;
+                function addText(text) {
+                    if (text)
+                        parts.push('"' + text.replace(/"/g, '\\"') + '"');
+                }
+                function addExpr(expressionText) {
+                    if (expressionText) {
+                        attrBinding = expressionText;
+                        parts.push('ko.unwrap(' + expressionText + ')');
+                    }
+                }
+                parseInterpolationMarkup(attr.value, addText, addExpr);
+
+                if (parts.length > 1) {
+                    attrBinding = '""+' + parts.join('+');
+                }
+
+                if (attrBinding) {
+                    attrBinding = 'attr.' + attr.name + ':' + attrBinding;
+                    if (!dataBindAttribute) {
+                        dataBindAttribute = attrBinding
+                    } else {
+                        dataBindAttribute += ',' + attrBinding;
+                    }
+                    node.setAttribute('data-bind', dataBindAttribute);
+                    node.removeAttributeNode(attr);
+                }
+            }
+        }
+    }
+}
+
+function enableAttributeInterpolationMarkup() {
+    setNodePreprocessor(attributeInterpolationMarkerPreprocessor);
+}
+
+var ko_punches_attributeInterpolationMarkup = ko_punches.attributeInterpolationMarkup = {
+    preprocessor: attributeInterpolationMarkerPreprocessor,
+    enable: enableAttributeInterpolationMarkup
 };
